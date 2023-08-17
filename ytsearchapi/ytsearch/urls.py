0391ab335dfd2e03210f883c6f5b1dfae8b7b3cd
aditiakhauri <akhauriaaditi@gmail.com>
2023-08-17 17:51:04 +0530

    YT commit

---
+++ ytsearchapi/ytsearch/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('fetchresults/<int:page>/', views.fetchstored, name='fetchstored'),
+    path('fetchresults/<str:query>/', views.fetchstored, name='fetchstored'),
+    path('fetchresults/<str:query>/<int:page>/', views.fetchstored, name='fetchstored'),
+    path('fetchresults/', views.fetchstored, name='fetchstored'),
+]
